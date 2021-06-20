ee1af55a25c8ace6a55759b0e57a2f97981512e8
christopher_windows <christopher_x10x@hotmail.com>
2021-06-20 17:22:43 -0500

    upgrading expo and adding react navigation 6x

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import Home from "./src/screens/Home";
+import SpringScreen from './src/screens/Spring'
 import { theme as appTheme } from "./src/constants";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const theme = {
   ...DefaultTheme,
@@ -13,10 +16,17 @@ const theme = {
   },
 };
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Spring" component={SpringScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </PaperProvider>
   );
