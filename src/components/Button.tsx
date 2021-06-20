9663e4dd61bd4c7adc9aef54ba91864b6902c814
christopher_windows <christopher_x10x@hotmail.com>
2021-06-20 15:57:49 -0500

    renaming and remove unnecesary propertys

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
 import { Button } from "react-native-paper";
 
-interface Buttonx {
+interface IButton {
     mode?: string,
     children?: React.ReactNode,
     onPress: () => any,
     style?: StyleProp<ViewStyle>
 }
 
-export default ({ mode = 'contained', children, onPress, style = {} }: Buttonx) => {
+export default ({ mode = 'contained', children, onPress, style = {} }: IButton) => {
 
     return (<Button mode="contained" onPress={onPress} style={[styles.button, style]}>
         {children}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,24 +4,35 @@ import { withTheme } from "react-native-paper";
 import { backgrounds } from "../constants/theme";
 import { Title, Button } from "../components"
 
+const Header = () => {
+  return (
+    <View style={styles.titleContainer}>
+      <Title title={"Home Screen!"} />
+    </View>
+  )
+}
+
+const Content = () => {
+  return (
+    <View style={styles.buttonsContainer}>
+      <Button onPress={() => console.log("Pressed1")}>
+        New Spring
+      </Button>
+      <Button
+        onPress={() => console.log("Pressed2")}
+      >
+        My Springs
+      </Button>
+    </View>
+  )
+}
+
 const Home: React.FC = (props: any) => {
   const { colors } = props.theme;
   return (
     <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Title title={"Home Screen!"} />
-      </View>
-      <View style={styles.buttonsContainer}>
-        <Button mode="contained" onPress={() => console.log("Pressed1")}>
-          New Spring
-        </Button>
-        <Button
-          mode="contained"
-          onPress={() => console.log("Pressed2")}
-        >
-          {"My Springs"}
-        </Button>
-      </View>
+      <Header />
+      <Content />
     </View>
   );
 };
